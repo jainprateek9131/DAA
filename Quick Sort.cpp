99924b1bdcc8712f9ccbd7884c8d256a0a06dbf6
jainprateek9131 <68363818+jainprateek9131@users.noreply.github.com>
2024-01-13 18:00:39 +0530

    Update Quick Sort.cpp

--- Quick Sort.cpp
+++ Quick Sort.cpp
@@ -1,3 +1,33 @@
+#include <iostream>
+using namespace std;
+int partition(int a[],int l,int r){
+    int pivot=r;
+    int ipivot=l;
+    for(int i=l;i<r;i++){
+        if(a[i]<=pivot){
+            swap(a[ipivot++],a[i]);
+        }
+    }
+    swap(a[ipivot],a[r]);return ipivot;
+}
+void quickSort(int a[],int l,int r){
+    if(l<r){
+        int ipivot = partition(a,l,r);
+        quickSort(a,l,ipivot-1);
+        quickSort(a,ipivot+1,r);
+    }
+}
+//merge sort
+int main() {
+    int a[]={4,2,5,6,4,9};
+    int n = sizeof(a)/sizeof(a[0]);
+    quickSort(a,0,n-1);
+    
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+	}
+	return 0;
+}
 #include <stdio.h>
 #include <bits/stdc++.h>
 using namespace std;
